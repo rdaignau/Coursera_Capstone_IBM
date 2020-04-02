22e9780c6824f863dba8673b0e18fc8820a7d715
rdaignau <63070792+rdaignau@users.noreply.github.com>
2020-04-02 15:43:55 -0400

    Add files via upload

---
+++ Capstone Project Notebook.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be use for the Capstone Project - Course 9 of 9 of the data science IBM certification
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
